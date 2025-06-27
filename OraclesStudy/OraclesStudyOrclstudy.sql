d29825cd14845bad0b26d7f1f5f6560ea37fa307
jeongin03 <jeongin.kwon523@gmail.com>
2025-06-27 14:40:39 +0900

    Delete OraclesStudy directory

--- OraclesStudy/OraclesStudyOrclstudy.sql
+++
@@ -1,4 +0,0 @@
--- conn orclstudy/oracle : orcl 접속
-select * from scott.temp; insert into scott.temp values('text', 'from orclstudy'); select * from scott.temp; -- orclstudy로 사용 권한을 부여받은 temp 테이블 사용하기
-
-select * from scott.temp; -- orclstudy로 권한 철회된 temp 테이블 조회하기 (실패)
